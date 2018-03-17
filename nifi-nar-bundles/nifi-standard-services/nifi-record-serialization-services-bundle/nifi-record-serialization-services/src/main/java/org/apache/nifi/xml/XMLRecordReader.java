ba971c23a5be0c9216eb7f3f2dcca7a10ab59741
JohannesDaniel <JohannesDaniel.Peter@gmx.de>
2018-03-17 15:06:53 +0100

    3

--- nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/main/java/org/apache/nifi/xml/XMLRecordReader.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/main/java/org/apache/nifi/xml/XMLRecordReader.java
@@ -2,10 +2,13 @@ package org.apache.nifi.xml;
 
 import org.apache.nifi.serialization.MalformedRecordException;
 import org.apache.nifi.serialization.RecordReader;
+import org.apache.nifi.serialization.record.DataType;
 import org.apache.nifi.serialization.record.MapRecord;
 import org.apache.nifi.serialization.record.Record;
 import org.apache.nifi.serialization.record.RecordField;
 import org.apache.nifi.serialization.record.RecordSchema;
+import org.apache.nifi.serialization.record.SerializedForm;
+import org.apache.nifi.serialization.record.type.RecordDataType;
 import org.apache.nifi.serialization.record.util.DataTypeUtils;
 import org.apache.nifi.util.Tuple;
 
@@ -13,6 +16,7 @@ import javax.xml.stream.XMLEventReader;
 import javax.xml.stream.XMLInputFactory;
 import javax.xml.stream.XMLStreamException;
 import javax.xml.stream.events.Attribute;
+import javax.xml.stream.events.Characters;
 import javax.xml.stream.events.EndElement;
 import javax.xml.stream.events.StartElement;
 import javax.xml.stream.events.XMLEvent;
@@ -48,11 +52,12 @@ public class XMLRecordReader implements RecordReader {
 
     // final zu allem wo es geht
 
-    public XMLRecordReader(InputStream in, RecordSchema schema, boolean isArray, String xmlPathToRecord,
+    public XMLRecordReader(InputStream in, RecordSchema schema, boolean isArray, String recordName,
                            final String dateFormat, final String timeFormat, final String timestampFormat) throws MalformedRecordException {
         this.in =  in;
         this.schema = schema;
         this.fieldNames = schema.getFieldNames();
+        this.recordName = recordName;
 
         final DateFormat df = dateFormat == null ? null : DataTypeUtils.getDateFormat(dateFormat);
         final DateFormat tf = timeFormat == null ? null : DataTypeUtils.getDateFormat(timeFormat);
@@ -61,11 +66,6 @@ public class XMLRecordReader implements RecordReader {
         LAZY_DATE_FORMAT = () -> df;
         LAZY_TIME_FORMAT = () -> tf;
         LAZY_TIMESTAMP_FORMAT = () -> tsf;
-
-        boolean evaluateXmlPathToRecord = xmlPathToRecord != null;
-        if (evaluateXmlPathToRecord) {
-
-        }
 
         try {
             XMLInputFactory xmlInputFactory = XMLInputFactory.newInstance();
@@ -76,13 +76,10 @@ public class XMLRecordReader implements RecordReader {
                 StartElement rootTag = getNextStartTag();
                 setNextRecordStartTag();
                 rootName = rootTag.getName().toString();
-                if (currentRecordStartTag != null) {
-                    recordName = currentRecordStartTag.getName().toString();
-                }
+
             } else {
                 StartElement rootTag = getNextStartTag();
                 rootName = getNextStartTag().getName().toString();
-                recordName = rootName;
                 currentRecordStartTag = rootTag;
             }
 
@@ -109,6 +106,7 @@ public class XMLRecordReader implements RecordReader {
                 StartElement startElement = xmlEvent.asStartElement();
                 if (startElement.getName().toString().equals(recordName)) {
                     currentRecordStartTag = startElement;
+                    return;
                 } else {
                     skipElement();
                 }
@@ -122,46 +120,68 @@ public class XMLRecordReader implements RecordReader {
         if (currentRecordStartTag == null) {
             return null;
         }
-
-        System.out.println("Start record");
         try {
             Record record = parseRecord(currentRecordStartTag, this.schema);
             setNextRecordStartTag();
-
-            System.out.println("Return record");
             return record;
         } catch (XMLStreamException e) {
             throw new MalformedRecordException("Could not parse XML", e);
         }
+    }
 
-
-        /*
-        try {
-
-            while(xmlEventReader.hasNext()){
-                XMLEvent xmlEvent = xmlEventReader.nextEvent();
-
-                if (xmlEvent.isStartElement()) {
-                    StartElement startElement = xmlEvent.asStartElement();
-                    System.out.println(startElement.getName());
-                    parseRecord(startElement);
-
-
-                } else if (xmlEvent.isEndElement()) {
-                    EndElement endElement = xmlEvent.asEndElement();
-                    System.out.println(endElement.getName());
-                }
+    private Object parseField(StartElement startElement, String fieldName, DataType dataType) throws XMLStreamException {
+        switch (dataType.getFieldType()) {
+            case BOOLEAN:
+            case BYTE:
+            case CHAR:
+            case DOUBLE:
+            case FLOAT:
+            case INT:
+            case LONG:
+            case SHORT:
+            case STRING:
+            case DATE:
+            case TIME:
+            case TIMESTAMP: {
+                final String value = extractSimpleValue(startElement);
+                return DataTypeUtils.convertType(value, dataType, LAZY_DATE_FORMAT, LAZY_TIME_FORMAT, LAZY_TIMESTAMP_FORMAT, fieldName);
             }
-        } catch (XMLStreamException e) {
-            e.printStackTrace();
+            case ARRAY: {
+
+            }
+            case RECORD: {
+                RecordSchema childSchema;
+                if (dataType instanceof RecordDataType) {
+                    childSchema = ((RecordDataType) dataType).getChildSchema();
+                } else {
+                    return null;
+                }
+                parseRecord(startElement, childSchema);
+
+            }
+            case MAP: {
+
+            }
+            case CHOICE: {
+
+            }
         }
-*/
+        return null;
+
+    }
+
+    private String extractSimpleValue(StartElement startElement) throws XMLStreamException {
+        // throw exception RecordSchemaForXmlException (extends XMLStreamException) man koennte isCharacters, isEndElement nutzen
+        XMLEvent xmlEvent;
+        Characters characters;
+        xmlEvent = xmlEventReader.nextEvent();
+        characters = xmlEvent.asCharacters();
+        xmlEventReader.nextEvent();
+        return characters.toString();
     }
 
     private Record parseRecord(StartElement startElement, RecordSchema schema) throws XMLStreamException {
-        Map<String, Object> recordValues = new HashMap<String, Object>();
-        //Record record = new MapRecord(schema, recordValues);
-
+        Map<String, Object> recordValues = new HashMap<>();
 
         // parse attributes
         Iterator iterator = startElement.getAttributes();
@@ -172,45 +192,27 @@ public class XMLRecordReader implements RecordReader {
         }
 
         // parse fields
-
         while(xmlEventReader.hasNext()){
             XMLEvent xmlEvent = xmlEventReader.nextEvent();
 
             if (xmlEvent.isStartElement()) {
                 StartElement startSubElement = xmlEvent.asStartElement();
-                Optional<RecordField> field = schema.getField(startSubElement.getName().toString());
+                String fieldName = startSubElement.getName().toString();
+                Optional<RecordField> field = schema.getField(fieldName);
                 if (field.isPresent()){
-                    // parse simple field
-
-                    // parse array
-
-                    // parse record
-
-                    System.out.println(startSubElement.getName());
+                    Object value = parseField(startSubElement, fieldName, field.get().getDataType());
+                    recordValues.put(fieldName, value);
                 } else {
                     skipElement();
                 }
             } else if (xmlEvent.isEndElement()) {
                 EndElement endElement = xmlEvent.asEndElement();
                 if (endElement.getName().equals(startElement.getName())) {
-                    return null;
+                    break;
                 }
             }
         }
-        return null;
-        /*
-            List<RecordField> fields = new ArrayList<RecordField>() {{
-                add(new RecordField("field_int", RecordFieldType.INT.getDataType()));
-                add(new RecordField("field_string", RecordFieldType.STRING.getDataType()));
-            }};
-
-            Map<String, Object> values = new HashMap<String, Object>(){{
-                put("field_int", 123);
-                put("field_string", "value_string");
-            }};
-
-            return new MapRecord(new SimpleRecordSchema(fields), values);
-         */
+        return new MapRecord(schema, recordValues);
     }
 
     private void skipElement() throws XMLStreamException {
@@ -226,16 +228,6 @@ public class XMLRecordReader implements RecordReader {
         }
     }
 
-    private Object parseSimpleField(StartElement startElement) throws XMLStreamException {
-        while(xmlEventReader.hasNext()){
-            XMLEvent xmlEvent = xmlEventReader.nextEvent();
-
-
-        }
-
-
-        return new RecordField("", null);
-    }
 
 
 
--- nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/xml/TestXMLRecordReader.java
+++ nifi-nar-bundles/nifi-standard-services/nifi-record-serialization-services-bundle/nifi-record-serialization-services/src/test/java/org/apache/nifi/xml/TestXMLRecordReader.java
@@ -3,9 +3,11 @@ package org.apache.nifi.xml;
 import org.apache.nifi.serialization.MalformedRecordException;
 import org.apache.nifi.serialization.SimpleRecordSchema;
 import org.apache.nifi.serialization.record.DataType;
+import org.apache.nifi.serialization.record.Record;
 import org.apache.nifi.serialization.record.RecordField;
 import org.apache.nifi.serialization.record.RecordFieldType;
 import org.apache.nifi.serialization.record.RecordSchema;
+import org.junit.Assert;
 import org.junit.Test;
 
 import java.io.ByteArrayInputStream;
@@ -30,22 +32,32 @@ public class TestXMLRecordReader {
     @Test(expected = MalformedRecordException.class)
     public void testEmptyStream() throws IOException, MalformedRecordException {
         InputStream is = new ByteArrayInputStream(new byte[0]);
-        XMLRecordReader reader = new XMLRecordReader(is, getSimpleSchema(), true, null, dateFormat, timeFormat, timestampFormat);
+        XMLRecordReader reader = new XMLRecordReader(is, getSimpleSchema(), true, "PERSON", dateFormat, timeFormat, timestampFormat);
         reader.nextRecord(true, true);
     }
 
     @Test
     public void testParseEmptyArray() throws IOException, MalformedRecordException {
         InputStream is = new ByteArrayInputStream("<root></root>".getBytes());
-        XMLRecordReader reader = new XMLRecordReader(is, getSimpleSchema(), true, null, dateFormat, timeFormat, timestampFormat);
+        XMLRecordReader reader = new XMLRecordReader(is, getSimpleSchema(), true, "PERSON", dateFormat, timeFormat, timestampFormat);
         assertEquals(reader.nextRecord(true, true), null);
     }
 
     @Test
     public void test() throws IOException, MalformedRecordException {
         InputStream is = new FileInputStream("src/test/resources/xml/people.xml");
-        XMLRecordReader rr = new XMLRecordReader(is, getSimpleSchema(), true, null, dateFormat, timeFormat, timestampFormat);
+        XMLRecordReader rr = new XMLRecordReader(is, getSimpleSchema(), true, "PERSON", dateFormat, timeFormat, timestampFormat);
         rr.nextRecord(true, true);
+    }
+
+    @Test
+    public void testParsingSimpleRecords() throws IOException, MalformedRecordException {
+        InputStream is = new FileInputStream("src/test/resources/xml/people.xml");
+        XMLRecordReader reader = new XMLRecordReader(is, getSimpleSchema(), true, "PERSON", dateFormat, timeFormat, timestampFormat);
+        Assert.assertArrayEquals(new Object[] {"Cleve Butler", 42, "USA"}, reader.nextRecord().getValues());
+        Assert.assertArrayEquals(new Object[] {"Ainslie Fletcher", 33, "UK"}, reader.nextRecord().getValues());
+        Assert.assertArrayEquals(new Object[] {"Amélie Bonfils", 74, "FR"}, reader.nextRecord().getValues());
+        Assert.assertArrayEquals(new Object[] {"Elenora Scrivens", 16, "USA"}, reader.nextRecord().getValues());
     }
 
     @Test
@@ -54,13 +66,16 @@ public class TestXMLRecordReader {
         XMLRecordReader rr = new XMLRecordReader(is, getSimpleSchema(), true, null, dateFormat, timeFormat, timestampFormat);
         rr.nextRecord(true, true);
     }
+
+
+
     @Test
     public void testNestedXml() throws IOException, MalformedRecordException {
         InputStream is = new FileInputStream("src/test/resources/xml/people_nested.xml");
-        RecordSchema schema = schemaWithNestedRecord();
+        RecordSchema schema = getSchemaWithNestedRecord();
 
 
-        XMLRecordReader rr = new XMLRecordReader(is, schema, true, null, dateFormat, timeFormat, timestampFormat);
+        XMLRecordReader rr = new XMLRecordReader(is, schema, true, "PERSON", dateFormat, timeFormat, timestampFormat);
 
 
         rr.nextRecord(true, true);
@@ -76,7 +91,7 @@ public class TestXMLRecordReader {
 
     private List<RecordField> getNestedRecordFields () {
         final List<RecordField> fields = new ArrayList<>();
-        fields.add(new RecordField("STREET", RecordFieldType.INT.getDataType()));
+        fields.add(new RecordField("STREET", RecordFieldType.STRING.getDataType()));
         fields.add(new RecordField("CITY", RecordFieldType.STRING.getDataType()));
         return fields;
     }
@@ -89,7 +104,7 @@ public class TestXMLRecordReader {
         return new SimpleRecordSchema(getNestedRecordFields());
     }
 
-    private RecordSchema schemaWithNestedRecord() {
+    private RecordSchema getSchemaWithNestedRecord() {
         final List<RecordField> fields = getSimpleRecordFields();
         final DataType dataType = RecordFieldType.RECORD.getRecordDataType(getNestedSchema());
         fields.add(new RecordField("ADDRESS", dataType));
